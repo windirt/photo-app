89addbcac5c8cc00e1dea492f1633c17a46e3499
Windirt Young <windirt@gmail.com>
2016-04-01 22:35:37 +0800

    try to fix mail setting

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
+
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :username => ENV['SENDGRID_USERNAME'],
+    :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
